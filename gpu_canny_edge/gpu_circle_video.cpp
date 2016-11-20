6967f5120e819a5d8b71ae9cc94201068caa05da
Elliot Sandfort <etsandfort@gmail.com>
2016-11-19 20:48:12 -0600

    slightly better circle detection

--- gpu_canny_edge/gpu_circle_video.cpp
+++ gpu_canny_edge/gpu_circle_video.cpp
@@ -15,9 +15,9 @@ int main(int argc, char *argv[]) {
     IplImage  *frame = NULL;
     IplImage  *grey  = NULL;
     IplImage  *edges = NULL;
-	int threshold_value=0, threshold_type=3;
+	int threshold_value=15, threshold_type=3;
 	int const max_value = 255;
-	int const max_type = 4;
+	int const max_type = 3;
 	int const max_BINARY_value = 255;
 
  
@@ -47,10 +47,13 @@ int main(int argc, char *argv[]) {
     /* calculate the delay between each frame and display video's FPS */
     delay = (int) (1000/cvGetCaptureProperty(video, CV_CAP_PROP_FPS));
 
-    
+    int frmCounter = 0;
     //FrameRateMonitor frm;
     //frm.Start();
     while (frame) {
+	if(frmCounter%4 == 0){
+		continue;
+	}
         //frm.MarkFrame();  
 	vector<Vec3f> circles;  
 	gpu::GpuMat tempFrame, tempGrey, tempEdges;
@@ -63,11 +66,12 @@ int main(int argc, char *argv[]) {
 	/* Edges on the input gray image (needs to be grayscale) using the Canny algorithm.
            Uses two threshold and a aperture parameter for Sobel operator. */
     gpu::cvtColor(tempFrame, tempGrey, CV_BGR2GRAY);
+	GaussianBlur( tempGrey, tempGrey, Size(9,9), 2, 2 );
 	threshold(tempGrey, tempEdges, threshold_value, max_BINARY_value,threshold_type);
        
  	
 	Mat e;
-	tempGrey.download(e);
+	tempEdges.download(e);
 	HoughCircles( e, circles, CV_HOUGH_GRADIENT, 1, tempGrey.rows/8, 200, 100, 0, 0 );
 	
   for( int i = 0; i< circles.size(); i++ )
@@ -75,15 +79,16 @@ int main(int argc, char *argv[]) {
 		      Point center(cvRound(circles[i][0]), cvRound(circles[i][1]));
       int radius = cvRound(circles[i][2]);
       // circle center
-      circle( e, center, 3, Scalar(0,255,0), -1, 8, 0 );
+      circle( e, center, 3, Scalar(255,0,0,255), -1, 8, 0 );
       // circle outline
-      circle( e, center, radius, Scalar(0,0,255), 3, 8, 0 );
+      circle( e, center, radius, Scalar(255,0,0,255), 3, 8, 0 );
      }
 	/* show loaded frame */
        	imshow(window_name, e);
 	
 	/* load and check next frame*/
         frame = cvQueryFrame(video);
+		frmCounter+=1;
 	if(!frame) {
           //  frm.Stop();
           // frm.DumpInfo();
