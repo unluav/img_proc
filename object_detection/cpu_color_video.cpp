feb0a13d0ea5afe33c5751b1ac7f86099b6d2077
Jessee Zhang <jzhang@unluav-dev.unl.edu>
2017-03-02 21:39:22 -0600

    adds notes and pointless printf's

--- object_detection/cpu_color_video.cpp
+++ object_detection/cpu_color_video.cpp
@@ -39,39 +39,39 @@ int main(int argc, char *argv[]) {
     /* Get the first frame and create a edges image with the same size */
     frame = cvQueryFrame(video);
     //grey  = cvCreateImage(cvGetSize(frame), IPL_DEPTH_8U, 1);
-    blobs = cvCreateImage(cvGetSize(frame), IPL_DEPTH_8U, 1);
+    blobs = cvCreateImage(cvGetSize(frame), IPL_DEPTH_8U, 1);			//empty image of the same size of the frame. this is where we store info from processing the frame
     /* calculate the delay between each frame and display video's FPS */
     delay = (int) (1000/cvGetCaptureProperty(video, CV_CAP_PROP_FPS));
     
-    Mat tempFrame, redBlobs1, redBlobs2,greenBlobs;
+    Mat tempFrame, redBlobs1, redBlobs2,greenBlobs;			//these are boolean matrices ... redBlobs2 is because we have to deal with red twice due to the color spectrum linear scale
     int frmCounter = 0;
     while (frame) {
 	frmCounter++;
 	if(frmCounter%3 == 0){
 		continue;
 	}
-        //frm.MarkFrame();  
-	vector<vector<Point> > redContours; 
+        //frm.MarkFrame();  //Daric's frame counter -- check gpu canny edge
+	vector<vector<Point> > redContours; 		//vector is a list in c#
 	vector<vector<Point> > greenContours;
-	vector<Vec4i> rHierarchy;
+	vector<Vec4i> rHierarchy;			//hierarchy?
 	vector<Vec4i> gHierarchy;
 	
 	//Creating a Mat version of the current frame
-	cvarrToMat(frame).copyTo(tempFrame);
-	cvarrToMat(blobs).copyTo(redBlobs1);
+	cvarrToMat(frame).copyTo(tempFrame);		//copy the crrent frame into a temp frame that we can edit
+	cvarrToMat(blobs).copyTo(redBlobs1);		//initializes the matrices of certain size
 	cvarrToMat(blobs).copyTo(redBlobs2);
 	cvarrToMat(blobs).copyTo(greenBlobs);
 	
 
 	/* Edges on the input gray image (needs to be grayscale) using the Canny algorithm.
            Uses two threshold and a aperture parameter for Sobel operator. */
-    cvtColor(tempFrame, redBlobs1, CV_BGR2HSV);
-    cvtColor(tempFrame, redBlobs2, CV_BGR2HSV);
+    cvtColor(tempFrame, redBlobs1, CV_BGR2HSV);		//copies tempFrame into redBlobs with color for edge detection
+    cvtColor(tempFrame, redBlobs2, CV_BGR2HSV);		//color filters
  	cvtColor(tempFrame, greenBlobs, CV_BGR2HSV);
  
 	/*Finding the color blobs (needs to be a colored image) using Inrange*/
 	inRange(redBlobs1, Scalar(160,80,80), Scalar(179,255,255),redBlobs1);
-	inRange(redBlobs2, Scalar(160,80,80), Scalar(179,255,255),redBlobs2);
+	inRange(redBlobs2, Scalar(0,80,80), Scalar(10,255,255),redBlobs2);
 	Mat redBlobs;
 	bitwise_or(redBlobs1, redBlobs2, redBlobs);
 	inRange(greenBlobs, Scalar(40,80,80), Scalar(80,255,255), greenBlobs);
@@ -86,7 +86,7 @@ int main(int argc, char *argv[]) {
     for( int i = 0; i< greenContours.size(); i++ )
     {
        Scalar color = Scalar( 120,255,255 );
-      drawContours( greenBlobs, greenContours, i, color, 2, 8, gHierarchy, 0, Point() );
+      drawContours( greenBlobs, greenContours, i, color, 2, 8, gHierarchy, 0, Point() );		//instead of using i, we could use -1, which would draw them all
     }
  	Mat disp;
  	bitwise_or(greenBlobs, redBlobs, disp);
@@ -103,6 +103,12 @@ int main(int argc, char *argv[]) {
  
 	/* wait delay and check for the quit key */
         key = cvWaitKey(delay);
-        if(key=='q') break;
+        if(key=='q'){
+		break;
+		printf("HERGEDY BLERGEDY YOU PRESSED Q\n");
+	}else {
+		printf("hi");
+	}
+	printf("hi");
     }
 }
