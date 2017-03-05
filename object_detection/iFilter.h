725ee1cf3cba253da0f1b164969bc991bb1ecd03
Jessee Zhang <jzhang@unluav-dev.unl.edu>
2017-03-05 17:37:29 -0600

    abstracts the video player in filters to make more modular

---
+++ object_detection/iFilter.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/core/core.hpp"
+
+using namespace cv; //TODO: EW EW EW
+//This is an abstract class for a filter object, which will be used in conjunction with the video player
+
+class IFilter {
+
+	public:
+		virtual cv::Mat processFrame(IplImage *frame) = 0;
+};
