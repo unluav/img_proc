f42c3ce3030e3adf4fd339903dbefce57e25a347
elliot <etsandfort@gmail.com>
2017-03-10 01:50:32 -0600

    basic display for tracking

--- object_detection/cpu_color_video.cpp
+++ object_detection/cpu_color_video.cpp
@@ -51,48 +51,54 @@ int main(int argc, char *argv[]) {
 		continue;
 	}
         //frm.MarkFrame();  
-	vector<vector<Point> > redContours; 
+	//vector<vector<Point> > redContours; 
 	vector<vector<Point> > greenContours;
 	vector<Vec4i> rHierarchy;
 	vector<Vec4i> gHierarchy;
 	
 	//Creating a Mat version of the current frame
 	cvarrToMat(frame).copyTo(tempFrame);
-	cvarrToMat(blobs).copyTo(redBlobs1);
-	cvarrToMat(blobs).copyTo(redBlobs2);
+	//cvarrToMat(blobs).copyTo(redBlobs1);
+	//cvarrToMat(blobs).copyTo(redBlobs2);
 	cvarrToMat(blobs).copyTo(greenBlobs);
 	
 
 	/* Edges on the input gray image (needs to be grayscale) using the Canny algorithm.
            Uses two threshold and a aperture parameter for Sobel operator. */
-    cvtColor(tempFrame, redBlobs1, CV_BGR2HSV);
-    cvtColor(tempFrame, redBlobs2, CV_BGR2HSV);
+    //cvtColor(tempFrame, redBlobs1, CV_BGR2HSV);
+    //cvtColor(tempFrame, redBlobs2, CV_BGR2HSV);
  	cvtColor(tempFrame, greenBlobs, CV_BGR2HSV);
  
 	/*Finding the color blobs (needs to be a colored image) using Inrange*/
-	inRange(redBlobs1, Scalar(160,80,80), Scalar(179,255,255),redBlobs1);
-	inRange(redBlobs2, Scalar(160,80,80), Scalar(179,255,255),redBlobs2);
-	Mat redBlobs;
-	bitwise_or(redBlobs1, redBlobs2, redBlobs);
+	//inRange(redBlobs1, Scalar(160,80,80), Scalar(179,255,255),redBlobs1);
+	//inRange(redBlobs2, Scalar(160,80,80), Scalar(179,255,255),redBlobs2);
+	//Mat redBlobs;
+	//bitwise_or(redBlobs1, redBlobs2, redBlobs);
 	inRange(greenBlobs, Scalar(40,80,80), Scalar(80,255,255), greenBlobs);
-// 	findContours(redBlobs, redContours, rHierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0));
+    // 	findContours(redBlobs, redContours, rHierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0));
  	findContours(greenBlobs, greenContours, gHierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0));
  	
-// 	for( int i = 0; i< redContours.size(); i++ )
- //   {
- //     Scalar color = Scalar( 0,255,255);
- //     drawContours( redBlobs, redContours, i, color, 2, 8, rHierarchy, 0, Point() );
- //   }
-    for( int i = 0; i< greenContours.size(); i++ )
-    {
-       Scalar color = Scalar( 120,255,255 );
+	vector<Moments> mu(greenContours.size());
+ 	for( int i = 0; i < greenContours.size(); i++ )
+    	{ mu[i] = moments( greenContours[i], false ); }
+
+    //  Get the mass centers:
+  	vector<Point2f> mc( greenContours.size() );
+   
+  	for( int i = 0; i < greenContours.size(); i++ )
+   	{ mc[i] = Point2f( mu[i].m10/mu[i].m00 , mu[i].m01/mu[i].m00 ); }
+    
+   	for( int i = 0; i< greenContours.size(); i++ )
+   	{
+       Scalar color = Scalar( 60,255,255 );
       drawContours( greenBlobs, greenContours, i, color, 2, 8, gHierarchy, 0, Point() );
-    }
+      circle( greenBlobs, mc[i], 4, color, -1, 8, 0 );
+   	}
  	Mat disp;
- 	bitwise_or(greenBlobs, redBlobs, disp);
-	
+ 	//bitwise_or(greenBlobs, redBlobs, disp);
+
 	/* show loaded frame */
-       	imshow(window_name, disp);
+       	imshow(window_name, greenBlobs);
 	
 	/* load and check next frame*/
         frame = cvQueryFrame(video);
@@ -100,7 +106,7 @@ int main(int argc, char *argv[]) {
 	    printf("error loading frame.\n");
 		return 1;
 	}
- 
+
 	/* wait delay and check for the quit key */
         key = cvWaitKey(delay);
         if(key=='q') break;
