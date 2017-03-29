9196d658af957dd152c857ccac98cd12297bc209
Jacob Koperski <jkoperski@unluav-dev.unl.edu>
2017-03-29 13:50:30 -0500

    Refactoring ConfidenceArcTester.cpp

--- roomba_tracking/ConfidenceArcTester.cpp
+++ roomba_tracking/ConfidenceArcTester.cpp
@@ -1,47 +1,12 @@
 #include <iostream>
-#include <string>
-#include "ConfidenceArc.hpp"
-#include <utility>
+#include <iomanip>
+#include "ConfidenceArc.h"
 
-void Print(cv::Point2f prev, cv::Point2f curr, Prediction prediction);
-
-int main() {
-	std::vector<cv::Point2f> path;
-	std::vector<double> distErrors;
-	std::vector<double> angleErrors;
-	cv::Point2f prev(0.0, 0.0);
-	cv::Point2f curr;
-	Prediction prediction;
-	std::pair<double, double> distStats = std::make_pair(0.0, 0.0);
-	std::pair<double, double> angleStats = std::make_pair(0.0, 0.0);
+void Print(Prediction* p) {
 	
-	std::cout << "Iterations: ";
-	int cap;
-	std::cin >> cap;
-
-	for (int i = 0; i < cap; i++) {
-		curr.x = i + (double) rand() / RAND_MAX;
-		curr.y = i + (double) rand() / RAND_MAX;
-
-		path.push_back(prev);
-		ConfidenceArc::predictPoint(prev, curr, &distStats, &angleStats, &distErrors, &angleErrors, &prediction);
-
-		std::cout << i << std::endl;
-		Print(prev, curr, prediction);
-
-		prev = curr;
-	}
-
-	return 0;
 }
 
-void Print(cv::Point2f prev, cv::Point2f curr, Prediction prediction) {
-	std::cout << "(" << prev.x << ", " << prev.y << ") -> ";
-	std::cout << "(" << curr.x << ", " << curr.y << ")" << std::endl;
-	std::cout << "Distance: " << prediction.dist << std::endl;
-	std::cout << "Angle: " << prediction.angle << std::endl;
-	std::cout << "Distance Error: " << prediction.distError << std::endl;
-	std::cout << "Angle Error: " << prediction.angleError << std::endl;
-	std::cout << "Confidence: " << ConfidenceArc::getConfidence(prediction.dist, prediction.distError, prediction.angleError);
-	std::cout << std::endl << std::endl << std::endl;
+int main(int argc, char** argv) {
+	
+	return 0;
 }
