237d4cfba8c557926abf36d16bf5b96840b801f2
jared.frenzel.77@gmail.com <jared.frenzel.77@gmail.com>
2017-05-21 15:17:15 -0500

    added a temporary framework for object detection

---
+++ object_detection/detection_framework.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <iostream>
+#include "detection_framework.hpp"
+
+using namespace cv;
+using namespace std;
+
+vector<Point2f> get_positions(IplImage* frame) {
+	vector<Point2f> result {(0,0),(1,1),(2,2)}
+	return result;
+}
