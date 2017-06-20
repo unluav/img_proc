63b4b2b5ffeeed27c0a011d029d9bdfa68954808
Jacob Koperski <jkoperski@unluav-dev.unl.edu>
2017-06-19 19:13:26 -0500

    Some more renaming and moving of code for improved readability

--- object_detection/cpu_color_video.cpp
+++ object_detection/cpu_color_video.cpp
@@ -8,14 +8,14 @@
 using namespace std;
 using namespace cv;
 
-int findLargest(int size, vector<float> radii, vector<Point2f> centers, float[] key_radii, Point2f[] key_centers) {
-	int flag = 0, count = 0;
+int findLargest(int size, vector<float> radii, vector<Point2f> centers, float key_radii[], Point2f key_centers[]) {
+	int flag = 0, count = 0, min_radius = 20; // min_radius in pixels
 
 	for (int i = 0; i < size; i++) {
 		flag = 0;
 
 		for (int j = 0; j < 5; j++) {
-			if (radii[i] > key_radii[j] && flag == 0 && radii[i] > 20) {
+			if (radii[i] > key_radii[j] && flag == 0 && radii[i] > min_radius) {
 				key_radii[j] = radii[i];
 				key_centers[j] = centers[i];
 				flag = 1;
@@ -27,7 +27,7 @@ int findLargest(int size, vector<float> radii, vector<Point2f> centers, float[] 
 	return count;
 }
 
-int CenterTracking(Point2f centers[], IplImage *frame) {
+int fetchCenters(Point2f centers[], IplImage *frame) {
 	// calculate size of array
 	IplImage *blobs = NULL;
 
@@ -39,10 +39,7 @@ int CenterTracking(Point2f centers[], IplImage *frame) {
 
 	blobs = cvCreateImage(cvGetSize(frame), IPL_DEPTH_8U, 1);
 	Mat tempFrame, redBlobs1, redBlobs2, greenBlobs;
-	vector<vector<Point>> redContours;
-	vector<vector<Point>> greenContours;
-	vector<Vec4i> rHierarchy;
-	vector<Vec4i> gHierarchy;
+	vector<vector<Point>> red_contours, green_contours;
 
 	// Creating a Mat version of the current frame
 	cvarrToMat(frame).copyTo(tempFrame);
@@ -63,34 +60,32 @@ int CenterTracking(Point2f centers[], IplImage *frame) {
 	Mat redBlobs;
 	bitwise_or(redBlobs1, redBlobs2, redBlobs);
 	inRange(greenBlobs, Scalar(50, 80, 80), Scalar(130, 255, 255), greenBlobs);
-	findContours(redBlobs, redContours, rHierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0));
-	findContours(greenBlobs, greenContours, gHierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0));
+
+	vector<Vec4i> red_hierarchy, green_hierarchy;
+	findContours(redBlobs, red_contours, red_hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0));
+	findContours(greenBlobs, green_contours, green_hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0));
 
 	// calculate min enclosing circles for every red and green contour
-	vector<vector<Point>> red_poly(redContours.size());
-	vector<vector<Point>> green_poly(greenContours.size());
-	vector<Point2f> red_centers(redContours.size());
-	vector<Point2f> green_centers(greenContours.size());
-	vector<float> red_radii(redContours.size());
-	vector<float> green_radii(greenContours.size());
+	int red_count = red_contours.size(), green_count = green_contours.size();
+	vector<vector<Point>> red_poly(red_count), green_poly(green_count);
+	vector<Point2f> red_centers(red_count), green_centers(green_count);
+	vector<float> red_radii(red_count), green_radii(green_count);
 
-	for (int i = 0; i < redContours.size(); i++) {
-		approxPolyDP(Mat(redContours[i]), red_poly[i], 3, true);
+	for (int i = 0; i < red_count; i++) {
+		approxPolyDP(Mat(red_contours[i]), red_poly[i], 3, true);
 		minEnclosingCircle((Mat) red_poly[i], red_centers[i], red_radii[i]);
 	}
 
-	for (int i = 0;i < greenContours.size(); i++) {
-		approxPolyDP(Mat(greenContours[i]), green_poly[i], 3, true);
+	for (int i = 0;i < green_count; i++) {
+		approxPolyDP(Mat(green_contours[i]), green_poly[i], 3, true);
 		minEnclosingCircle((Mat) green_poly[i], green_centers[i], green_radii[i]);
 	}
 
-	float key_red_radii[5] = {};
-	float key_green_radii[5] = {};
-	Point2f key_red_centers[5];
-	Point2f key_green_centers[5];
+	float key_red_radii[5] = {}, key_green_radii[5] = {};
+	Point2f key_red_centers[5],  key_green_centers[5];
 
-	int red_count = findLargest(redContours.size(), red_radii, red_centers, key_red_radii, key_red_centers);
-	int green_count = findLargest(greenContours.size(), green_radii, green_centers, key_green_radii, key_green_centers);
+	red_count = findLargest(red_count, red_radii, red_centers, key_red_radii, key_red_centers);
+	green_count = findLargest(green_count, green_radii, green_centers, key_green_radii, key_green_centers);
 	int total_count = red_count + green_count;
 
 	// combine red and green arrays into one array and fill rest of array with -1,-1
