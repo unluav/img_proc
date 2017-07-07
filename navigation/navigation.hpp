03ba698ef2230dad25412fc85ae1c9bfb2bf4035
Jared Frenzel <jared.frenzel.77@gmail.com>
2017-07-06 19:09:21 -0500

    build fixes

--- navigation/navigation.hpp
+++ navigation/navigation.hpp
@@ -10,7 +10,7 @@
 #define     MAX(a,b)                        ( ( (a) > (b) ) ? (a) : (b) )
 #define     DIST_FROM_ORG(pt)               ( sqrt( pow( (pt).x, 2 ) + pow( (pt).y, 2 ) ) )
 #define     SPEED_CALCULATION(conf,dist)    ((double) MAX(MAX_SPEED_DISTANCE, dist) / (double) MAX_SPEED_DISTANCE) * conf  //May need to change this, I can't remember what we agreed upon...
-#define     THETA_CALCULATION(pt)           ( arctan( (double)(pt).x / (double)(pt).y ) )
+#define     THETA_CALCULATION(pt)           ( atan( (double)(pt).x / (double)(pt).y ) )
 
 #include    <thread>
 #include    <mutex>
