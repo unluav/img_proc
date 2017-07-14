630968468496686b2e938c8ae550c68635f2c825
JakeKo <wijakoperski@gmail.com>
2017-07-13 20:00:08 -0500

    Adding some pointer arithmetic

--- tracking/conf_test.cpp
+++ tracking/conf_test.cpp
@@ -6,22 +6,22 @@ using namespace cv;
 #define RAND_CAP 0.01
 
 // Just a helpful printer
-void printFrame(vector<Point2f> centers, vector<ConfidenceArc> arcs, int frame) {
+void printFrame(vector<Point2f>* centers, vector<ConfidenceArc>* arcs, int frame) {
 	printf("\n\n************************ FRAME %d ************************\n", frame);
 	
-	for (int i = 0; i < centers.size(); i++) {
-		Prediction pred = *arcs[i].getPrediction();
-		printf("A: (%.3f, %.3f)\t->\t", centers[i].x, centers[i].y);
-		printf("P: (%.3f, %.3f)\t[ %.6f%% ]\n", pred.point.x, pred.point.y, pred.confidence * 100);
+	for (int i = 0; i < centers->size(); i++) {
+		Prediction* pred = (*arcs)[i].getPrediction();
+		printf("A: (%.3f, %.3f)\t->\t", (*centers)[i].x, (*centers[i]).y);
+		printf("P: (%.3f, %.3f)\t[ %.6f%% ]\n", pred->point.x, pred->point.y, pred->confidence * 100);
 	}
 }
 
 // Pseudo-randomly generates the next frame
 // More of a placeholder for passing in a frame to ConfidenceArc
-void getNextFrame(vector<Point2f> centers, int frame) {
-	for (int i = 0; i < centers.size(); i++) {
-		centers[i].x = frame + i + (double) rand() / RAND_MAX * RAND_CAP;
-		centers[i].y = frame + i + (double) rand() / RAND_MAX * RAND_CAP;
+void getNextFrame(vector<Point2f>* centers, int frame) {
+	for (int i = 0; i < centers->size(); i++) {
+		(*centers)[i].x = frame + i + (double) rand() / RAND_MAX * RAND_CAP;
+		(*centers)[i].y = frame + i + (double) rand() / RAND_MAX * RAND_CAP;
 	}
 }
 
@@ -31,8 +31,8 @@ int main(int argc, char** argv) {
 	vector<ConfidenceArc> arcs(obj_count);
 
 	for (int i = 0; i < frames; i++) {
-		getNextFrame(centers, i);
-		printFrame(centers, arcs, i + 1);
+		getNextFrame(&centers, i);
+		printFrame(&centers, &arcs, i + 1);
 		ConfidenceArc::predictNextFrame(&centers, &arcs);
 	}
 
